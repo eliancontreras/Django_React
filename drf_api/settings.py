2fff2f7325918d1ceb4f4ba49d5e17e60e9331df
eliancontreras <contreraselian094@gmail.com>
2023-04-12 12:02:14 +0100

    update cloudinary settings and replace secret key with secure secret key

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -20,7 +20,7 @@ CLOUDINARY_STORAGE = {
     'CLOUDINARY_URL': os.environ.get('CLOUDINARY_URL')
 }
 MEDIA_URL = '/media/'
-DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaClourdinaryStorage'
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -31,7 +31,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-sf=b-1j_cyy0hgviswm!%-3ohr&r4)8)$sl%ih685yaxz43twq'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
--- profiles/models.py
+++ profiles/models.py
@@ -12,7 +12,7 @@ class Profile(models.Model):
     name = models.CharField(max_length=255, blank=True)
     content = models.TextField(blank=True)
     image = models.ImageField(
-        upload_to='images/', default='../default_profile_qdjgp'
+        upload_to='images/', default='../default_profile_odk5vv.jpeg'
     )
 
     class Meta:
