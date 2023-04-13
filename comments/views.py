225ab3bb25ca1e0590751a74139bb7a92c29ea1b
eliancontreras <contreraselian094@gmail.com>
2023-04-13 16:49:31 +0100

    add comments views, urls and serializers

--- comments/views.py
+++ comments/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status, permissions
+from .models import Comment
+from rest_framework import generics, permissions
+from .serializers import CommentSerializer, CommentDetailSerializer
+from drf_api.permissions import IsOwnerOrReadOnly
 
 # Create your views here.
+
+
+class CommentsList(generics.ListCreateAPIView):
+    serializer_class = CommentSerializer
+    permission_classes = [
+        permissions.IsAuthenticatedOrReadOnly
+    ]
+    queryset = Comment.objects.all()
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class CommentDetail(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = CommentDetailSerializer
+    permission_classes = [
+        IsOwnerOrReadOnly
+    ]
+    queryset = Comment.objects.all()
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
--- drf_api/urls.py
+++ drf_api/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),
     path('', include('profiles.urls')),
     path('posts/', include('posts.urls')),
+    path('comments/', include('comments.urls')),
 ]
