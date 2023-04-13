583402ac7d5a34a2cd4301a0bc52c143b2dacfba
eliancontreras <contreraselian094@gmail.com>
2023-04-13 10:47:42 +0100

    validate picture upload size in serializer

--- posts/serializers.py
+++ posts/serializers.py
@@ -2,11 +2,26 @@ from rest_framework import serializers
 from .models import Post
 
 
-class ProfileSerializer(serializers.ModelSerializer):
+class PostSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
     profile_id = serializers.ReadOnlyField(source='owner.profile.id')
     profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
+
+    def validate_image(self, value):
+        if value.size > 1024 * 1024 * 2:
+            raise serializers.ValidationError(
+                'Image size larger than 2MB!'
+            )
+        if value.image.width > 4096:
+            raise serializers.ValidationError(
+                'Image width larger than 4096px'
+            )
+        if value.image.height > 4096:
+            raise serializers.ValidationError(
+                'Image height larger than 4096px'
+            )
+        return value
 
     def get_is_owner(self, obj):
         request = self.context['request']
@@ -15,5 +30,7 @@ class ProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
         fields = [
-            'id', 'owner', 'created_at', 'updated_at', 'title', 'content', 'image', 'is_owner',
+            'id', 'owner', 'created_at',
+            'updated_at', 'title', 'content',
+            'image', 'is_owner', 'image_filter'
         ]
--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import Post
+from .serializers import PostSerializer
+# from drf_api.permissions import IsOwnerOrReadOnly
 
 # Create your views here.
+
+
+class PostsList(APIView):
+    def get(self, request):
+        posts = Post.objects.all()
+        serialzer = PostSerializer(
+            posts,
+            many=True,
+            context={'request': request}
+        )
+        return Response(serialzer.data)
