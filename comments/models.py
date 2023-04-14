8477d83d25336fdd5b836031e95c27c3013ffdc7
eliancontreras <contreraselian094@gmail.com>
2023-04-14 15:18:49 +0100

    add comments_count and likes_count to posts and detail posts

--- comments/models.py
+++ comments/models.py
@@ -9,7 +9,8 @@ class Comment(models.Model):
     Comment model, related to User and Post
     """
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
-    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    post = models.ForeignKey(
+        Post, related_name='comments', on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     content = models.TextField()
--- posts/views.py
+++ posts/views.py
@@ -1,17 +1,28 @@
-from rest_framework import generics, permissions
+from django.db.models import Count
+from rest_framework import generics, permissions, filters
 from drf_api.permissions import IsOwnerOrReadOnly
 from .models import Post
 from .serializers import PostSerializer
 
 
-class PostList(generics.ListCreateAPIView):
+class PostsList(generics.ListCreateAPIView):
     """
     List posts or create a post if logged in
     The perform_create method associates the post with the logged in user.
     """
+    queryset = Post.objects.annotate(
+        comments_count=Count('comments', distinct=True),
+        likes_count=Count('likes', distinct=True),
+    ).order_by('-created_at')
     serializer_class = PostSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
-    queryset = Post.objects.all()
+    filter_backends = [
+        filters.OrderingFilter
+    ]
+    ordering_fields = [
+        'comments_count',
+        'likes_count',
+    ]
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
@@ -21,6 +32,9 @@ class PostDetail(generics.RetrieveUpdateDestroyAPIView):
     """
     Retrieve a post and edit or delete it if you own it.
     """
+    queryset = Post.objects.annotate(
+        comments_count=Count('comments', distinct=True),
+        likes_count=Count('likes', distinct=True),
+    ).order_by('-created_at')
     serializer_class = PostSerializer
     permission_classes = [IsOwnerOrReadOnly]
-    queryset = Post.objects.all()
