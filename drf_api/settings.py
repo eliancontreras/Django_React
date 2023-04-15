2e3cd3890ab4f4f40e953bada73cad47ce642453
eliancontreras <contreraselian094@gmail.com>
2023-04-15 11:34:55 +0100

    set allowed host to env variable set on heroku

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -63,7 +63,10 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEV' in os.environ
 
-ALLOWED_HOSTS = ['localhost', 'moments-photos.herokuapp.com']
+ALLOWED_HOSTS = [
+    os.environ.get('ALLOWED_HOST'),
+    'localhost',
+]
 
 
 # Application definition
