092b333a4fb38f779e542579ef27e87177c8280c
eliancontreras <contreraselian094@gmail.com>
2023-04-15 11:00:24 +0100

    test live db

--- env.py
+++ env.py
@@ -3,7 +3,7 @@ os.environ['CLOUDINARY_URL'] = 'cloudinary://554799797989625:uTJImOV-oSNlJyv2q6A
 
 os.environ['SECRET_KEY'] = 'l%p+$nyt&3&cn31s_)&85y-@l5(4k*12_hy+1v7w7v522^i1dc'
 
-os.environ['DEV'] = '1'
+# os.environ['DEV'] = '1'
 
 os.environ.setdefault(
     "DATABASE_URL", "postgres://ouaauiwy:2aWN0cZsvBnsFVd7jK4YBrrF5m7-ece1@rogue.db.elephantsql.com/ouaauiwy")
