33a6e528e9a161f0641a7440047f8a5f8da8c037
eliancontreras <contreraselian094@gmail.com>
2023-04-18 15:56:04 +0100

    set CORS to all

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -115,11 +115,12 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-if 'CLIENT_ORIGIN' in os.environ:
-    CORS_ALLOWED_ORIGINS = [
-        os.environ.get('CLIENT_ORIGIN'),
-        os.environ.get('CLIENT_ORIGIN_DEV')
-    ]
+CORS_ORIGIN_ALLOW_ALL = True
+# if 'CLIENT_ORIGIN' in os.environ:
+#     CORS_ALLOWED_ORIGINS = [
+#         os.environ.get('CLIENT_ORIGIN'),
+#         os.environ.get('CLIENT_ORIGIN_DEV')
+#     ]
 
 # if 'CLIENT_ORIGIN_DEV' in os.environ:
 #     CORS_ALLOWED_ORIGINS = [
--- env.py
+++ env.py
@@ -5,7 +5,7 @@ os.environ['SECRET_KEY'] = 'l%p+$nyt&3&cn31s_)&85y-@l5(4k*12_hy+1v7w7v522^i1dc'
 
 os.environ['DEV'] = '1'
 
-# os.environ['ALLOWED_HOST'] = 'moments-photos.herokuapp.com'
+os.environ['ALLOWED_HOST'] = 'moments-photos.herokuapp.com'
 
 os.environ.setdefault(
     "DATABASE_URL", "postgres://ouaauiwy:2aWN0cZsvBnsFVd7jK4YBrrF5m7-ece1@rogue.db.elephantsql.com/ouaauiwy")
