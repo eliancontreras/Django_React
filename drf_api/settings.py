bfe4f483b13294cb09634647fba5d782c6d34b83
eliancontreras <contreraselian094@gmail.com>
2023-04-19 11:41:42 +0100

    CORS

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -68,18 +68,8 @@ ALLOWED_HOSTS = [
     'localhost',
 ]
 
-CORS_ORIGIN_WHITELIST = []
-# CORS_ORIGIN_ALLOW_ALL = True
-# if 'CLIENT_ORIGIN' in os.environ:
-#     CORS_ALLOWED_ORIGINS = [
-#         os.environ.get('CLIENT_ORIGIN'),
-#         os.environ.get('CLIENT_ORIGIN_DEV')
-#     ]
 
-# if 'CLIENT_ORIGIN_DEV' in os.environ:
-#     CORS_ALLOWED_ORIGINS = [
-#         os.environ.get('CLIENT_ORIGIN_DEV')
-#     ]
+CORS_ORIGIN_ALLOW_ALL = True
 
 
 CORS_ALLOW_CREDENTIALS = True
