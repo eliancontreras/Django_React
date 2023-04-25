23f32f3274a755721f6ec3f67c19ff081c5ba09c
eliancontreras <contreraselian094@gmail.com>
2023-04-25 12:30:50 +0100

    correct profile url

--- profiles/urls.py
+++ profiles/urls.py
@@ -3,5 +3,5 @@ from profiles import views
 
 urlpatterns = [
     path('', views.ProfilesList.as_view()),
-    path('profiles/<int:pk>/', views.ProfileDetail.as_view())
+    path('<int:pk>/', views.ProfileDetail.as_view())
 ]
